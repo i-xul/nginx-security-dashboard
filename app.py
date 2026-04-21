12d8e22b21062b887edbd6d639117d74bc045d88
Henkka <121262594+i-xul@users.noreply.github.com>
2026-04-21 22:24:58 +0300

    port updated

--- app.py
+++ app.py
@@ -61,4 +61,9 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    import os
+
+    port = int(os.environ.get("PORT", 5002))
+    host = os.environ.get("HOST", "0.0.0.0")
+
+    app.run(debug=True, host=host, port=port)
